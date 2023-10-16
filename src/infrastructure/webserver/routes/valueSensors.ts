fc813233612a777fa73b26343e357eada1a6eaff
pramudya0406 <aqshalpramudya01@gmail.com>
2023-10-16 15:01:30 +0700

    adding jwt and for deploy

--- src/infrastructure/webserver/routes/valueSensors.ts
+++ src/infrastructure/webserver/routes/valueSensors.ts
@@ -2,11 +2,13 @@ import express from "express";
 import valueSensorRepository from "../../../domain/repository/sensorValue";
 import valueSensorDbRepository from "../../mongoDb/db/sensor";
 import sensorValueController from "../../../controller/sensorValue/sensorValueController";
+import authMiddleware from "../../middleware/token";
+
 const router = express.Router();
 
 const controller = sensorValueController(valueSensorRepository, valueSensorDbRepository);
 
-router.get("/", controller.getValueSensor);
+router.get("/", authMiddleware, controller.getValueSensor);
 
 
 export default router;
