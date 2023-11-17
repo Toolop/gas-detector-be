517dad35f1ceb35f3cc32922dc44286a97f6600e
rafi <toolop2312@gmail.com>
2023-11-17 16:27:43 +0700

    fixing env for mqtt

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -1,6 +1,6 @@
 export default {
   port: process.env.PORT || 8080,
-  ip: process.env.HOST || "backend",
+  ip: process.env.HOST || "localhost",
   jwtSecret: process.env.JWT_SECRET || "jkl!±@£!@ghj1237",
-  mongodb: process.env.MONGODB || "mongodb://aqms:aqms@mongodb:27017",
+  mongodb: process.env.MONGODB || "mongodb://127.0.0.1:27017/gasdetector",
 };
--- backend/src/controller/client/condition.ts
+++ backend/src/controller/client/condition.ts
@@ -1,38 +1,35 @@
 const mqtt = require("mqtt");
-const clientId = `mqttScofindo_${Math.random().toString(16).slice(3)}`;
-const connectUrl = `mqtt://broker.hivemq.com:1883/mqtt`;
+const dotenv = require("dotenv");
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient({});
 
-const client = mqtt.connect(connectUrl, {
-  clientId,
-  keepalive: 30,
-  protocolId: "MQTT",
-  protocolVersion: 4,
-  clean: true,
-  connectTimeout: 30 * 1000,
-  rejectUnauthorized: false,
-  reconnectPeriod: 1000,
-});
-
 const publishCondtion = async () => {
   try {
-    const topic = "sucofindo/change/condition";
+    dotenv.config();
+
+    const clientId = `${process.env.clientMqtt}${Math.random()
+      .toString(16)
+      .slice(3)}`;
+
+    const client = await mqtt.connect(process.env.mqttUrl, {
+      clientId,
+      keepalive: 30,
+      protocolId: "MQTT",
+      protocolVersion: 4,
+      clean: true,
+      connectTimeout: 30 * 1000,
+      rejectUnauthorized: false,
+      reconnectPeriod: 1000,
+      username: process.env.usernameMqtt,
+      password: process.env.passwordMqtt,
+    });
+
+    const topic = process.env.topicCondition;
 
     let message = await prisma.condition.findMany({});
 
-    client.publish(
-      topic,
-      JSON.stringify(message),
-      { qos: 0, retain: false },
-      function (error: any) {
-        if (error) {
-        } else {
-          console.log("Published");
-        }
-      }
-    );
+    client.publish(topic, JSON.stringify(message), { qos: 0, retain: false });
   } catch (err) {
     console.log(err);
   }
